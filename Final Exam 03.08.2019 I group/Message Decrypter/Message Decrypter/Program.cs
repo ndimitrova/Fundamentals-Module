cf1dbe53a73c2a2ea1633a3f6d2a3d622966d477
ndimitrova <37267538+ndimitrova@users.noreply.github.com>
2020-11-18 17:18:19 +0200

    Add files via upload

---
+++ Final Exam 03.08.2019 I group/Message Decrypter/Message Decrypter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Message_Decrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"^([$]|[%])(?<tag>[A-Z][a-z][a-z]+)\1[:] [[](?<first>[0-9]+)][|][[](?<second>[0-9]+)][|][[](?<third>[0-9]+)][|]$");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    Console.WriteLine($"{match.Groups["tag"].Value}: {(char)int.Parse(match.Groups["first"].Value)}{(char)int.Parse(match.Groups["second"].Value)}{(char)int.Parse(match.Groups["third"].Value)}");
+                }
+
+                else
+                {
+                    Console.WriteLine($"Valid message not found!");
+                }
+            }
+        }
+    }
+}
